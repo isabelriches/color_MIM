48a7c924ab8d91dead238e87dbe5ab8d561f2ca6
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-06 16:53:05 -0500

    Update color_check_neighbors.m
    I didn't realize I was supposed to annotate the versions of my work, but this took me approximately 2 hours of research and 1 hour coding and debugging. I need to fix the displaying of these colors, then will change this.

--- color_check_neighbors.m
+++ color_check_neighbors.m
@@ -5,7 +5,6 @@ lightgreen  = [0.4660, 0.6740, 0.1880];
 cyan        = [0.2, 0.8, 0.8];
 purple      = [0.4940, 0.1840, 0.5560];
 
-
 if current_gen(m,n) == 1
 			if (neighbors < 2 || neighbors >3)
                 		new_gen(m,n) = 0;
@@ -26,5 +25,3 @@ if current_gen(m,n) == 1
             		end
             
         	end
-
-
