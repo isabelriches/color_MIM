7d88343b948ae311d0104d04d8798b3fbba3d031
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-06 20:50:55 -0500

    Update color_check_neighbors.m
    I spent 4 hours researching and fixing my code, debugging along the way while I check its functionality in Matlab. I plan to go to office hours tomorrow to get more help initializing my code.

--- color_check_neighbors.m
+++ color_check_neighbors.m
@@ -1,27 +1,67 @@
-function [] = color_check_neighbors()
+function [new_gen] = color_check_neighbors(current_gen)
 
 darkred     = [0.6350, 0.0780, 0.1840];
 lightgreen  = [0.4660, 0.6740, 0.1880];
 cyan        = [0.2, 0.8, 0.8];
 purple      = [0.4940, 0.1840, 0.5560];
 
-if current_gen(m,n) == 1
+new_gen = zeros(100);
+for m = 2:99 %for rows 1 through 100
+	for n = 2:99 %for columns 1 through 100
+        neighbors = 0;
+	%count number of neighbors%
+		if current_gen(m-1,n-1) == 1
+            		neighbors = neighbors+1;
+        	end
+	
+		if current_gen(m-1,n) == 1
+			neighbors = neighbors+1;
+		end
+	
+		if current_gen(m-1,n+1) == 1
+   			neighbors = neighbors+1;
+		end
+	
+		if current_gen(m,n-1) == 1
+			neighbors = neighbors+1;
+		end
+	
+		if current_gen(m,n+1) == 1
+			neighbors = neighbors+1;
+		end
+	
+		if current_gen(m+1,n-1) == 1
+			neighbors = neighbors+1;
+		end
+	
+		if current_gen(m+1,n) == 1
+			neighbors = neighbors+1;
+		end
+		
+		if current_gen(m+1,n+1) == 1
+           		neighbors = neighbors+1;
+        	end
+        
+       		%determine if cells should live or die
+        	if current_gen(m,n) == 1
 			if (neighbors < 2 || neighbors >3)
                 		new_gen(m,n) = 0;
-				disp darkred;
             		end
             
             		if (neighbors == 2 || neighbors == 3)
                 		new_gen(m,n) = 1;
-				disp lightgreen;
             		end 
         	elseif current_gen(m,n) == 0
             		if neighbors == 3
                 		new_gen(m,n) = 1;
-				disp cyan;
             		else
                 		new_gen(m,n) = 0;
-				disp purple;
             		end
             
         	end
+    end
+end
+
+color_show_stuff(new_gen);
+axis off
+end
