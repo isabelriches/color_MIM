6ae0dda3cdd9ae68f78d0c85087e6c68f7e15535
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-07 17:51:12 -0500

    Update color_check_neighbors.m
    I spent 6 and 1/2 hours debugging and rewriting the code for this, and got it to finally change colors in the manner I was hoping for. I also spent time collaborating with my group via Gathertown regarding our project and discussed my code with them.

--- color_check_neighbors.m
+++ color_check_neighbors.m
@@ -1,9 +1,9 @@
 function [new_gen] = color_check_neighbors(current_gen)
 
-darkred     = [0.6350, 0.0780, 0.1840];
-lightgreen  = [0.4660, 0.6740, 0.1880];
-cyan        = [0.2, 0.8, 0.8];
-purple      = [0.4940, 0.1840, 0.5560];
+yellow      = [1, 1, 0];
+lightblue   = [0, 0.45, 0.75];
+purple      = [0.5, 0.2, 0.5];
+
 
 new_gen = zeros(100);
 for m = 2:99 %for rows 1 through 100
@@ -45,17 +45,21 @@ for m = 2:99 %for rows 1 through 100
        		%determine if cells should live or die
         	if current_gen(m,n) == 1
 			if (neighbors < 2 || neighbors >3)
-                		new_gen(m,n) = 0;
+                		new_gen(m,n) = 0.2;
+                       
             		end
             
             		if (neighbors == 2 || neighbors == 3)
                 		new_gen(m,n) = 1;
+                       
             		end 
         	elseif current_gen(m,n) == 0
             		if neighbors == 3
                 		new_gen(m,n) = 1;
+                      
             		else
                 		new_gen(m,n) = 0;
+                        
             		end
             
         	end
