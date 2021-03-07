5616a61d5e47e5757c6109063dab876f8f41cd0c
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-06 20:52:56 -0500

    Update color_game_of_life.m
    I spent 4 hours researching and fixing my code, debugging along the way while I check its functionality in Matlab. I plan to go to office hours tomorrow to get more help initializing my code.

--- color_game_of_life.m
+++ color_game_of_life.m
@@ -1,9 +1,10 @@
 function [] = color_game_of_life()
+close all
 current_gen = randi([0,1],100);
 imagesc(current_gen); %creates an initial generation matrix of random 0’s and 1’s
 
 while true
-    [new_gen] = check_neighbors(current_gen);
+    [new_gen] = color_check_neighbors(current_gen);
     current_gen = new_gen;
 end
 
