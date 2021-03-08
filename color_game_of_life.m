eb5c14f1768cea2be9ef3b65830895b9902c3a25
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-08 10:53:11 -0500

    Update color_game_of_life.m
    I commented out "% imagesc(current_gen); %creates an initial generation matrix of random 0’s and 1’s" and replaced this with a new variable I called "colorswitch" which creates a 3D matrix that randomizes the 'current_gen' while taking in the 'colorswitch' matrix I created. This took me 3 hours to research and teach myself about, and then another 1 1/2 hours to implement and debug.

--- color_game_of_life.m
+++ color_game_of_life.m
@@ -1,7 +1,13 @@
+
 function [] = color_game_of_life()
 close all
-current_gen = randi([0,1],100);
-imagesc(current_gen); %creates an initial generation matrix of random 0’s and 1’s
+
+colorswitch = randi([0,1],100,100,3); 
+current_gen = cat(3, colorswitch, colorswitch, colorswitch);
+
+channel_yellow = current_gen(:,:,1);
+channel_lightblue = current_gen(:,:,2);
+channel_purple = current_gen(:,:,3);
 
 while true
     [new_gen] = color_check_neighbors(current_gen);
