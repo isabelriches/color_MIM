77dcc2bde2eeb214db465cd5b1537d5c6d53dc9f
Matt Murphy <77451610+matt-j-murphy@users.noreply.github.com>
2021-03-09 10:12:34 -0500

    Update color_game_of_life.m
    Matt
    Commenting out unused code

--- color_game_of_life.m
+++ color_game_of_life.m
@@ -3,11 +3,11 @@ function [] = color_game_of_life()
 close all
 
 colorswitch = randi([0,1],100,100,3); 
-current_gen = cat(3, colorswitch, colorswitch, colorswitch);
+%current_gen = cat(3, colorswitch, colorswitch, colorswitch);
 
-channel_yellow = current_gen(:,:,1);
-channel_lightblue = current_gen(:,:,2);
-channel_purple = current_gen(:,:,3);
+%channel_yellow = current_gen(:,:,1);
+%channel_lightblue = current_gen(:,:,2); 
+%channel_purple = current_gen(:,:,3);
 
 while true
     [new_gen] = color_check_neighbors(current_gen);
