1e1544b9d0bb5c0d5b1abdb796c1fb50be970764
Matt Murphy <77451610+matt-j-murphy@users.noreply.github.com>
2021-03-09 08:43:26 -0500

    Update color_check_neighbors.m
    Matt
    15 minutes
    Comments/formatting

--- color_check_neighbors.m
+++ color_check_neighbors.m
@@ -44,24 +44,19 @@ for m = 2:99 %for rows 1 through 100
         
        		%determine if cells should live or die
         	if current_gen(m,n) == 1
-			if (neighbors < 2 || neighbors >3)
+			if (neighbors < 2 || neighbors >3) %if a 1's cell has less than 2 or greater than 3 neighbors, it turns to a 0.2 cell
                 		new_gen(m,n) = 0.2;
-                       
             		end
             
-            		if (neighbors == 2 || neighbors == 3)
+            		if (neighbors == 2 || neighbors == 3) %if a 1's cell has 2 or 3 neighbors, it stays 1
                 		new_gen(m,n) = 1;
-                       
             		end 
-        	elseif current_gen(m,n) == 0
-            		if neighbors == 3
-                		new_gen(m,n) = 1;
-                      
+        	elseif current_gen(m,n) == 0 
+            		if neighbors == 3 %if a dead/non-existent cell has 3 neighbors, it comes alive
+				new_gen(m,n) = 1;
             		else
                 		new_gen(m,n) = 0;
-                        
             		end
-            
         	end
     end
 end
@@ -69,7 +64,7 @@ end
 color_show_stuff(new_gen);
 
 new_gen(randi([2,99],1,1),randi([2,99],1,1)) = 1;
-new_gen(randi([2,99],1,1),randi([2,99],1,1)) = 1;
+new_gen(randi([2,99],1,1),randi([2,99],1,1)) = 1;  %adds additional yellows (1's) to each generation
 new_gen(randi([2,99],1,1),randi([2,99],1,1)) = 1;
 
 axis off
