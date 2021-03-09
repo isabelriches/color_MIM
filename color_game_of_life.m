2f4d94fba59f5561769821cce33c47e1975ed666
isabelriches <79932085+isabelriches@users.noreply.github.com>
2021-03-09 10:43:34 -0500

    Update color_game_of_life.m
    Commented 'current_gen = cat(3, colorswitch, colorswitch, colorswitch);' back in, as it is imperative to the running of the game.

--- color_game_of_life.m
+++ color_game_of_life.m
@@ -3,7 +3,7 @@ function [] = color_game_of_life()
 close all
 
 colorswitch = randi([0,1],100,100,3); 
-%current_gen = cat(3, colorswitch, colorswitch, colorswitch);
+current_gen = cat(3, colorswitch, colorswitch, colorswitch);
 
 %channel_yellow = current_gen(:,:,1);
 %channel_lightblue = current_gen(:,:,2); 
